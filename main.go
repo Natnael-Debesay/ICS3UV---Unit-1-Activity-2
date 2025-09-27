6ef9fd539437c41fa8375b834e2429299dda5f13
default <natnael.debesay@stu.ocsb.ca>
2025-09-27 22:15:09 +0000

    main.ts

---
+++ main.go
@@ -0,0 +1,14 @@
+/*
+ * Author: Natnael Debesay
+ * Version: 1.0.0
+ * Date: 2025-09-27
+ * This is the traditional "Hello, World!" program.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
---
+++ main.ts
@@ -0,0 +1,11 @@
+ /**
+  * @author Natnael Debesay
+  * @version 1.0.0
+  * @date 2025-09-27
+  * @fileoverview This is the traditional "Hello, World!" program.
+  */
+
+// print out the greeting
+console.log("Hello, World!")
+
+console.log("/nDone.");
