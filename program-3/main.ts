b3fa7c3833459a0ae6b9a52c943bc0ce897f2a9b
default <natnael.debesay@stu.ocsb.ca>
2025-09-29 20:44:45 +0000

    main.ts

---
+++ program-3/main.ts
@@ -0,0 +1,20 @@
+/**
+ * @author Natnael Debesay
+ * @version 1.0.0
+ * @date 2025-09-29
+ * @fileoverview This program displays the Happy Birthday song.
+ */
+
+// string literal text
+console.log("Happy Birthday to you.");
+
+// string literal text
+console.log("Happy birthday to you.");
+
+// string literal text
+console.log("Happy birthday dear Natnael.");
+
+// string liter text
+console.log("Happy birthday to you.");
+
+console.log("\nDone.")
