c562e6608dbc25776f6dc58bcfa9d50c3f2f6c89
default <natnael.debesay@stu.ocsb.ca>
2025-09-29 21:05:33 +0000

    main.go

--- program-3/main.go
+++ program-3/main.go
@@ -1,1 +1,27 @@
-package program3
+/* 
+ * @author Natnael Debesay
+ * @version 1.0.0
+ * @date 2025-09-29
+ * @fileoverview This program displays the Happy Birthday song.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	//string literal text
+	fmt.Println("Happy birthday to you.")
+
+	//string literal text
+	fmt.Println("Happy birthday to you.")
+
+	//string literal text
+	fmt.Println("Happy birthday dear Natnael.")
+
+	//string literal text
+	fmt.Println("Happy birthday to you.")
+
+	fmt.Println("\nDone.")
+}
