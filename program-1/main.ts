22bfb22895fbbd4411896fb9fb0af3c70d5da13c
default <natnael.debesay@stu.ocsb.ca>
2025-09-29 21:16:03 +0000

    main.ts

--- program-1/main.ts
+++ program-1/main.ts
@@ -5,19 +5,19 @@
  * @fileoverview This program displays the names of the couses I am taking this semester.
  */
 
-//string literal text
+// string literal text
 console.log("Natnael Debesay");
 
-//string literal text
+// string literal text
 console.log("TWJE31 Custom Woodworking");
 
-//string literal text
-console.log("NBE3UR English-University - Contemporary First Nations, Metis and Inuit Voices");
+// string literal text
+console.log("NBE3UR English - University - Contemporary First Nations, Metis and Inuit Voices");
 
-//string literal text
+// string literal text
 console.log("ICS3UV Introduction to Computer Programming");
 
-//string literal text
+// string literal text
 console.log("PAL3OR Large Group Activities - Basketball and Volleyball");
 
 console.log("\nDone.");
